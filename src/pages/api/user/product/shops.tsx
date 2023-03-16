12a32e01554e85cd6a2d7e735eef892bdd206f17
Esterling Accime <accimeesterlin@yahoo.com>
2023-03-16 02:32:13 -0700

    adding shops

--- src/pages/api/user/product/shops.tsx
+++ src/pages/api/user/product/shops.tsx
@@ -1,13 +1,12 @@
 import nc from "next-connect";
 import { getProductByShopId } from "pages/api/queries/getProduct";
-// import { getLatestShops } from "pages/api/queries/getShop";
+import { getLatestShops } from "pages/api/queries/getShop";
 
 export const findProductHandler = async (req, res) => {
   try {
     const query = req?.query;
     console.log("Query: ", query);
-    // const shops: any = await getLatestShops();
-    const shops: any = []
+    const shops: any = await getLatestShops();
 
     const products = await Promise.all(
       shops?.map(async (shop) => {
