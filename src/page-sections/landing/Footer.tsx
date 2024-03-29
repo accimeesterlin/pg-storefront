bacff8a9ba519800d6194d62209af9f62b84fad5
Esterling Accime <accimeesterlin@yahoo.com>
2024-03-28 22:07:13 -0700

    Fix formatting in Footer component

--- src/page-sections/landing/Footer.tsx
+++ src/page-sections/landing/Footer.tsx
@@ -28,11 +28,16 @@ const Footer: FC = () => {
 
         <FlexBox className="flex">
           {iconList.map((item) => (
-            <a href={item.url} target="_blank" rel="noreferrer noopenner" key={item.iconName}>
+            <a
+              href={item.url}
+              target="_blank"
+              rel="noreferrer noopenner"
+              key={item.iconName}
+            >
               <Icon size="1.25rem" defaultcolor="auto" mx="0.5rem">
                 {item.iconName}
               </Icon>
-             
+            </a>
           ))}
         </FlexBox>
       </FlexBox>
@@ -43,7 +48,10 @@ const Footer: FC = () => {
 const iconList = [
   { iconName: "facebook-1", url: "https://www.facebook.com/UILibOfficial" },
   { iconName: "twitter-1", url: "/" },
-  { iconName: "youtube-1", url: "https://www.youtube.com/channel/UCsIyD-TSO1wQFz-n2Y4i3Rg" },
+  {
+    iconName: "youtube-1",
+    url: "https://www.youtube.com/channel/UCsIyD-TSO1wQFz-n2Y4i3Rg",
+  },
   { iconName: "instagram-1", url: "/" },
 ];
 
